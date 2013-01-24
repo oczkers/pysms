d8dccc1022da9dddb1b5c0d0400a0b8c6cac2070
oczkers <oczkers@gmail.com>
2013-01-24 15:12:09 +0100

    pep8

--- pysms/__init__.py
+++ pysms/__init__.py
@@ -2,10 +2,20 @@
 # -*- coding: utf-8 -*-
 import re
 import requests
+from operator import play
+
 
 def getMNC(cell):
-	''' return Mobile Network Code ( http://en.wikipedia.org/wiki/Mobile_Network_Code ) '''
-	opera = requests.session()
-	values = { 'msisdn':cell }	# remember about prefix
-	content = opera.post('http://download.t-mobile.pl/updir/updir.cgi', values).content
-	return re.search('<b>Kod sieci:</b></td><td>[0-9]+ ([0-9]+)</td>', content).group(1)
+    ''' return Mobile Network Code ( http://en.wikipedia.org/wiki/Mobile_Network_Code ) '''
+    opera = requests.session()
+    data = {'msisdn': cell}    # remember about prefix
+    content = opera.post('http://download.t-mobile.pl/updir/updir.cgi', data).content
+    return re.search('<b>Kod sieci:</b></td><td>[0-9]+ ([0-9]+)</td>', content).group(1)
+
+
+def sendSMS(cell):
+    ''' send SMS '''
+    mnc = getMNC(cell)
+    if mnc == '06':
+        network = play
+    return network.sendSMS(cell, 'test')    # login?
--- pysms/operator/play.py
+++ pysms/operator/play.py
@@ -1,29 +1,42 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
 import requests
 
+
 def login(username, passwd):
-	opera = requests.session()
-	content = opera.get('https://bramka.play.pl/composer/public/mmsCompose.do').content
-	SAMLRequest = re.search('value="(.*?)"', content, re.MULTILINE|re.DOTALL).group(1)
-	content = opera.post('https://logowanie.play.pl/p4-idp2/SSOrequest.do', {'SAMLRequest':SAMLRequest, 'target':'https://bramka.playmobile.pl'}).content
-	random = re.search('name="random" value="(.+)"', content).group(1)
-	content = opera.post('https://logowanie.play.pl/p4-idp2/Login.do', {'step':1, 'random':random, 'login':username, 'password':passwd}).content
-	SAMLResponse = re.search('value="(.+?)"', content, re.MULTILINE|re.DOTALL).group(1)
-	content = opera.post('https://bramka.play.pl/composer/samlLog?action=sso', {'SAMLResponse':SAMLResponse, 'target':'https://bramka.playmobile.pl'}).content
-	content = opera.post('https://bramka.play.pl/composer/j_security_check', {'SAMLResponse':SAMLResponse}).content
-	return opera
+    '''Returns requests session after login'''
+    opera = requests.session()
+    content = opera.get('https://bramka.play.pl/composer/public/mmsCompose.do').content
+    SAMLRequest = re.search('value="(.*?)"', content, re.MULTILINE|re.DOTALL).group(1)
+    data = {'SAMLRequest': SAMLRequest,
+            'target': 'https://bramka.playmobile.pl'}
+    content = opera.post('https://logowanie.play.pl/p4-idp2/SSOrequest.do', data).content
+    random = re.search('name="random" value="(.+)"', content).group(1)
+    data = {'step': 1, 'random': random, 'login': username,
+            'password': passwd}
+    content = opera.post('https://logowanie.play.pl/p4-idp2/Login.do', data).content
+    SAMLResponse = re.search('value="(.+?)"', content, re.MULTILINE|re.DOTALL).group(1)
+    data = {'SAMLResponse': SAMLResponse,
+            'target': 'https://bramka.playmobile.pl'}
+    content = opera.post('https://bramka.play.pl/composer/samlLog?action=sso', data).content
+    data = {'SAMLResponse': SAMLResponse}
+    content = opera.post('https://bramka.play.pl/composer/j_security_check', data).content
+    return opera
+
 
 def sendSMS(username, passwd, cell, text):
-	opera = login(username, passwd)
-	content = opera.get('https://bramka.play.pl/composer/public/editableSmsCompose.do').content
-	randForm = re.search('name="randForm" value="(.+)"', content).group(1)
-	data = {'recipients':cell, 'content_in':text, 'czas':0, 'templateId':'', 'sendform':'on', 'composedMsg':'', 'randForm':randForm, 'old_signature':'', 'old_content':text, 'content_out':text}
-	opera.post('https://bramka.play.pl/composer/public/editableSmsCompose.do', data).content
-	data['SMS_SEND_CONFIRMED'] = 'Wyślij'
-	content = opera.post('https://bramka.play.pl/composer/public/editableSmsCompose.do', data).content
-	if 'Wiadomość została wysłana' in content:
-		return True
-	else:
-		return False
+    '''Returns True if sms is sent'''
+    opera = login(username, passwd)
+    content = opera.get('https://bramka.play.pl/composer/public/editableSmsCompose.do').content
+    randForm = re.search('name="randForm" value="(.+)"', content).group(1)
+    data = {'recipients': cell, 'content_in': text, 'czas': 0, 'templateId': '',
+            'sendform': 'on', 'composedMsg': '', 'randForm': randForm,
+            'old_signature': '', 'old_content': text, 'content_out': text}
+    opera.post('https://bramka.play.pl/composer/public/editableSmsCompose.do', data).content
+    data['SMS_SEND_CONFIRMED'] = 'Wyślij'
+    content = opera.post('https://bramka.play.pl/composer/public/editableSmsCompose.do', data).content
+    if 'Wiadomość została wysłana' in content:
+        return True
+    else:
+        return False
