ce94115df5de4ca8e2261c3c6c83476f70496b8f
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-04-21 09:27:10 +0000

    move tests

---
+++ test_pysms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for pysms."""
+
+import unittest
+
+import pysms
+
+
+class PysmsTestCase(unittest.TestCase):
+
+    #_multiprocess_can_split_ = True
+
+    def setUp(self):
+        """Setup."""
+        #self.modules = pysms.operator.__all__
+        pass
+
+    def tearDown(self):
+        """Teardown."""
+        pass
+
+    # def testEntryPoints(self):
+    #     gdown.getUrl
+    #     gdown.getFile
+    #     gdown.upload
+    #     gdown.expireDate
+
+
+if __name__ == '__main__':
+    unittest.main()
